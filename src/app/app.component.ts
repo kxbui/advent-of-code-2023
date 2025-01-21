d64ff30d5e8f5a7ae7dc731f7172d7231ceaa924
Khoi Bui <emquacau123472@yahoo.com>
2025-01-20 20:19:10 -0500

    day 1 part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,69 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, NgZone, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'advent-of-code-2023';
+  input = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`;
+
+  result = signal('');
+  ngZone = inject(NgZone);
+
+  onSubmit() {
+    this.result.set(`...waiting`);
+
+    this.ngZone.runOutsideAngular(() => {
+      setTimeout(() => {
+        const data = this.parseRow(this.input);
+        const total = this.start(data);
+        this.result.set(`${total}`);
+      }, 0);
+    });
+  }
+
+  start(arr: string[]): number {
+    let first = '',
+      last = '',
+      total = 0;
+    arr.forEach((line) => {
+      for (let i = 0; i < line.length; i++) {
+        if (this.isNumeric(line.at(i))) {
+          first = first === '' ? line.at(i)! : first;
+          last = line.at(i)!;
+        }
+      }
+      total += Number(`${first}${last}`);
+      first = '';
+      last = ''
+    });
+    return total;
+  }
+
+  isNumeric(str: string | undefined): boolean {
+    return str ? /^\d+$/.test(str) : false;
+  }
+
+  parseRow(data: any): any[] {
+    return data.split(/\r?\n|\r|\n/g);
+  }
+
+  parseColumn(data: any): { arr1: any[]; arr2: any[] } {
+    const arr1: any[] = [],
+      arr2: any[] = [];
+    const separateLines = data.split(/\r?\n|\r|\n/g);
+    separateLines.forEach((element: any) => {
+      const stringArray = element.trim().split(/\s*[\s,]\s*/);
+      arr1.push(+stringArray[0]);
+      arr2.push(+stringArray[1]);
+    });
+    return { arr1, arr2 };
+  }
 }
