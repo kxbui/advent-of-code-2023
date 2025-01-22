93090cd563ba1356cf0e44577ab5452a7c69ca13
Khoi Bui <emquacau123472@yahoo.com>
2025-01-22 11:03:51 -0500

    day 3 part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,16 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+  input = `467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`;
 
   result = signal('');
   ngZone = inject(NgZone);
@@ -23,44 +28,70 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
 
     this.ngZone.runOutsideAngular(() => {
       setTimeout(() => {
-        const data = this.parseRow(this.input);
+        const data = this.parseRow(this.input).map((line) => line.split(''));
         const total = this.start(data);
         this.result.set(`${total}`);
       }, 0);
     });
   }
 
-  start(arr: string[]): number {
+  start(map: string[][]): number {
+    let digit = '';
     let total = 0;
-    arr.forEach((line) => {
-      const [_, sets] = line.split(':');
-      total += this.getMinCubes(sets);
-    });
+    let isIncluded = false;
+    for (let r = 0; r < map.length; r++) {
+      for (let c = 0; c < map[0].length; c++) {
+        if (this.isDigit(map[r][c])) {
+          digit += map[r][c];
+          isIncluded = !isIncluded
+            ? this.checkNeighbors(map, { row: r, col: c })
+            : isIncluded;
+        } else {
+          total += isIncluded ? Number(digit) : 0;
+          digit = '';
+          isIncluded = false;
+        }
+      }
+    }
     return total;
   }
 
-  getMinCubes(sets: string): number {
-    let b = 0,
-    r = 0,
-    g = 0;
-
-    sets.split(';').forEach((set) => {
-      set.split(',').forEach((cube) => {
-        const blue = cube.includes('blue') ? this.getDigit(cube) : null;
-        const red = cube.includes('red') ? this.getDigit(cube) : null;
-        const green = cube.includes('green') ? this.getDigit(cube) : null;
-
-        b = blue !== null && blue > b ? blue : b;
-        r = red !== null && red > r ? red : r;
-        g = green !== null && green > g ? green : g;
-      });
-    }, 0);
-
-    return (b ?? 1) * (r ?? 1) * (g ?? 1);
+  checkNeighbors(map: string[][], curr: { row: number; col: number }): boolean {
+    return [
+      // left
+      { row: curr.row, col: curr.col - 1 },
+      // right
+      { row: curr.row, col: curr.col + 1 },
+      // top
+      { row: curr.row - 1, col: curr.col },
+      // bottom
+      { row: curr.row + 1, col: curr.col },
+      // top-left
+      { row: curr.row - 1, col: curr.col - 1 },
+      // top-right
+      { row: curr.row - 1, col: curr.col + 1 },
+      // bottom-left
+      { row: curr.row + 1, col: curr.col - 1 },
+      // bottom-right
+      { row: curr.row + 1, col: curr.col + 1 },
+    ].some((loc) => this.isSymbol(map, loc));
   }
 
-  getDigit(str: string): number {
-    return parseInt(str.replace(/^\D+/g, ''));
+  isSymbol(map: string[][], curr: { row: number; col: number }): boolean {
+    const width = map[0].length;
+    const height = map.length;
+    return (
+      curr.row >= 0 &&
+      curr.row < height &&
+      curr.col >= 0 &&
+      curr.col < width &&
+      map[curr.row][curr.col] !== '.' &&
+      !this.isDigit(map[curr.row][curr.col])
+    );
+  }
+
+  isDigit(str: string): boolean {
+    return !Number.isNaN(parseInt(str.replace(/^\D+/g, '')));
   }
 
   parseRow(data: any): any[] {
