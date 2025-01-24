45ce9f90f010759efe0596f5bdee245617d2879a
Khoi Bui <emquacau123472@yahoo.com>
2025-01-23 21:26:52 -0500

    day 6 part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,39 +9,8 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  input = `seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4`;
+  input = `Time:      7  15   30
+Distance:  9  40  200`;
 
   result = signal('');
   ngZone = inject(NgZone);
@@ -59,72 +28,40 @@ humidity-to-location map:
   }
 
   start(data: string[]): number {
-    let min = Infinity;
-    let dest = -1;
+    const arr = this.parseInput(data);
+    let total = 1;
 
-    const { seeds, maps } = this.parseInput(data);
-
-    seeds.forEach((seed) => {
-      dest = seed;
-      maps.forEach((map) => {
-        dest = this.mapValue(map, dest);
-      });
-      if (min > dest) min = dest;
+    arr.forEach((item) => {
+      total *= this.countOptions(item);
     });
-    return min;
+    return total;
   }
 
-  mapValue(map: any[], source: number): number {
-    const section = map.find(
-      (sec) => source >= sec.sourceStart && source <= sec.sourceEnd
-    );
-    if (section) {
-      const gap = source - section.sourceStart;
-      return section.destStart + gap;
-    }
-    return source
-  }
-
-  parseInput(input: string[]): { seeds: any[]; maps: any[] } {
-    const seeds = this.parseSeeds(input[0]);
-
-    const maps = this.parseConversion(input.slice(2));
-
-    return { seeds, maps };
-  }
-
-  parseConversion(data: string[]): any[] {
-    let arr: any[] = [],
-      maps: any[] = [];
+  countOptions(race: { time: number; distance: number }): number {
     let count = 0;
 
-    while (count <= data.length) {
-      const line = data[count];
-      if (line && line.trim()) {
-        if (!Number.isNaN(this.getDigit(line))) {
-          const [dest, source, range] = line.split(/\s*[\s,]\s*/);
-          arr.push({
-            sourceStart: Number(source),
-            sourceEnd: Number(source) + Number(range),
-            destStart: Number(dest),
-            destEnd: Number(dest) + Number(range),
-          });
-        }
-      } else {
-        maps.push(arr);
-        arr = [];
-      }
-      count++;
+    for (let i = 0; i <= race.time; i++) {
+      const dist = (race.time - i) * i;
+      if (dist > race.distance) count++;
     }
-    return maps;
+
+    return count;
   }
 
-  parseSeeds(line: string): any[] {
-    const [_, seeds] = line.split(':');
-    return seeds
+  parseInput(data: string[]): { time: number; distance: number }[] {
+    const [_t, timeStr] = data[0].split(':');
+    const timeArr = timeStr
       .trim()
       .split(/\s*[\s,]\s*/)
       .map((str) => this.getDigit(str));
+
+    const [_d, distStr] = data[1].split(':');
+    const distanceArr = distStr
+      .trim()
+      .split(/\s*[\s,]\s*/)
+      .map((str) => this.getDigit(str));
+
+    return timeArr.map((time, i) => ({ time, distance: distanceArr[i] }));
   }
 
   getDigit(str: string): number {
