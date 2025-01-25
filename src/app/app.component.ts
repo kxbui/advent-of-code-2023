2a96971b7e01074feed463e6e084c50876c4c67f
Khoi Bui <emquacau123472@yahoo.com>
2025-01-25 13:10:50 -0500

    day 8 part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 const LEFT = 'L';
 const RIGHT = 'R';
 
+// 21838660345787142275147567 high
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,11 +13,16 @@ const RIGHT = 'R';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  input = `LLR
+  input = `LR
 
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)`;
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`;
 
   result = signal('');
   ngZone = inject(NgZone);
@@ -35,22 +41,51 @@ ZZZ = (ZZZ, ZZZ)`;
 
   start(data: string[]): number {
     const { seq, network } = this.parseInput(data);
-    return this.lookup(seq, network);
+    return this.lookupMultiple(seq, network);
   }
 
-  lookup(seq: string, network: Map<string, any>): number {
+  lookupMultiple(seq: string, network: Map<string, any>): number {
+    let startNodes: string[] = Array.from(network.keys()).filter((key) =>
+      key.endsWith('A')
+    );
+    const arr = startNodes.map((node) => this.lookup(seq, network, node));
+    return this.findLcm(arr);
+  }
+
+  lookup(seq: string, network: Map<string, any>, startNode: string): number {
     let count = 0;
-    let start = 'AAA';
+    let start = startNode;
     while (true) {
       let i = 0;
       while (i < seq.length) {
         const val = network.get(start);
         start = seq[i] === LEFT ? val.left : val.right;
         count++;
-        if (start === 'ZZZ') return count;
+        if (start.endsWith('Z')) {
+          return count;
+        }
         i++;
       }
     }
+  }
+
+  findLcm(arr: number[]) {
+    let result = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      result = this.lcm(result, arr[i]);
+    }
+    return result;
+  }
+
+  lcm(a: number, b: number) {
+    return Math.abs(a * b) / this.gcd(a, b);
+  }
+
+  gcd(a: number, b: number): number {
+    if (b === 0) {
+      return a;
+    }
+    return this.gcd(b, a % b);
   }
 
   parseInput(data: string[]) {
