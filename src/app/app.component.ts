ca5aabe273d7231b005b5cfeb03d57bd4415fa9f
Khoi Bui <emquacau123472@yahoo.com>
2025-02-02 09:52:06 -0500

    day 14 part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,12 @@ import { FormsModule } from '@angular/forms';
 const ROUNDED = 'O';
 const CUBED = '#';
 const EMPTY = '.';
+const NUM_CYCLES = 1000000000;
 
+/**
+ * There're repeating patterns
+ * find the cycle length and skip ahead
+ */
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -40,28 +45,60 @@ O.#..O.#.#
   }
 
   start(data: string[][]): number {
-    const map = this.tiltPlatform(data);
-    return this.countTotalLoad(map);
+    const map = new Map<number, Set<string>>();
+    let count = 0,
+      curr = null,
+      patternStartIdx = -1;
+
+    while (patternStartIdx < 0) {
+      const result = this.tiltPlatform(data);
+      curr = this.getRounded(result);
+      patternStartIdx = this.patternExists(map, curr);
+      if (patternStartIdx < 0) {
+        map.set(count, curr);
+        count++;
+      }
+    }
+    const patterns = Array.from(map.values()).slice(patternStartIdx);
+    const idx = (NUM_CYCLES - patternStartIdx - 1) % patterns.length;
+    return this.countTotalLoad(patterns[idx], data.length);
   }
 
-  countTotalLoad(data: string[][]): number {
+  patternExists(
+    map: Map<number, Set<string>>,
+    curr: Set<string> | null
+  ): number {
+    return curr
+      ? Array.from(map.values()).findIndex((set) =>
+          this.areSetsEqual(set, curr)
+        )
+      : -1;
+  }
+
+  countTotalLoad(set: Set<string>, length: number): number {
     let total = 0;
 
-    for (let r = 0; r < data.length; r++) {
-      let rocks = 0;
-      for (let c = 0; c < data[0].length; c++) {
-        rocks += data[r][c] === ROUNDED ? 1 : 0;
-      }
-      total += rocks * (data.length - r);
-    }
+    Array.from(set.values()).forEach((rock) => {
+      const [row] = rock.split('-');
+      total += (length - Number(row));
+    });
     return total;
   }
 
   tiltPlatform(map: string[][]): string[][] {
+    let data = this.tiltNorth(map);
+    data = this.tiltWest(data);
+    data = this.tiltSouth(data);
+    data = this.tiltEast(data);
+
+    return data;
+  }
+
+  tiltNorth(map: string[][]): string[][] {
     for (let c = 0; c < map[0].length; c++) {
       for (let r = 1; r < map.length; r++) {
         if (map[r][c] === ROUNDED) {
-          const empty = this.findEmptySpace(map, { row: r, col: c });
+          const empty = this.findNorthEmptySpace(map, { row: r, col: c });
           if (empty) {
             map = this.swap(
               map,
@@ -75,7 +112,61 @@ O.#..O.#.#
     return map;
   }
 
-  findEmptySpace(
+  tiltWest(map: string[][]): string[][] {
+    for (let r = 0; r < map.length; r++) {
+      for (let c = 1; c < map[0].length; c++) {
+        if (map[r][c] === ROUNDED) {
+          const empty = this.findWestEmptySpace(map, { row: r, col: c });
+          if (empty) {
+            map = this.swap(
+              map,
+              { row: r, col: c },
+              { row: empty.row, col: empty.col }
+            );
+          }
+        }
+      }
+    }
+    return map;
+  }
+
+  tiltSouth(map: string[][]): string[][] {
+    for (let c = 0; c < map[0].length; c++) {
+      for (let r = map.length - 2; r >= 0; r--) {
+        if (map[r][c] === ROUNDED) {
+          const empty = this.findSouthEmptySpace(map, { row: r, col: c });
+          if (empty) {
+            map = this.swap(
+              map,
+              { row: r, col: c },
+              { row: empty.row, col: empty.col }
+            );
+          }
+        }
+      }
+    }
+    return map;
+  }
+
+  tiltEast(map: string[][]): string[][] {
+    for (let r = 0; r < map.length; r++) {
+      for (let c = map[0].length - 2; c >= 0; c--) {
+        if (map[r][c] === ROUNDED) {
+          const empty = this.findEastEmptySpace(map, { row: r, col: c });
+          if (empty) {
+            map = this.swap(
+              map,
+              { row: r, col: c },
+              { row: empty.row, col: empty.col }
+            );
+          }
+        }
+      }
+    }
+    return map;
+  }
+
+  findNorthEmptySpace(
     map: string[][],
     curr: { row: number; col: number }
   ): { row: number; col: number } | null {
@@ -91,6 +182,54 @@ O.#..O.#.#
     return result;
   }
 
+  findWestEmptySpace(
+    map: string[][],
+    curr: { row: number; col: number }
+  ): { row: number; col: number } | null {
+    let result = null;
+
+    for (let c = curr.col - 1; c >= 0; c--) {
+      if ([CUBED, ROUNDED].includes(map[curr.row][c])) {
+        return result;
+      } else if (map[curr.row][c] === EMPTY) {
+        result = { row: curr.row, col: c };
+      }
+    }
+    return result;
+  }
+
+  findSouthEmptySpace(
+    map: string[][],
+    curr: { row: number; col: number }
+  ): { row: number; col: number } | null {
+    let result = null;
+
+    for (let r = curr.row + 1; r < map.length; r++) {
+      if ([CUBED, ROUNDED].includes(map[r][curr.col])) {
+        return result;
+      } else if (map[r][curr.col] === EMPTY) {
+        result = { row: r, col: curr.col };
+      }
+    }
+    return result;
+  }
+
+  findEastEmptySpace(
+    map: string[][],
+    curr: { row: number; col: number }
+  ): { row: number; col: number } | null {
+    let result = null;
+
+    for (let c = curr.col + 1; c < map[0].length; c++) {
+      if ([CUBED, ROUNDED].includes(map[curr.row][c])) {
+        return result;
+      } else if (map[curr.row][c] === EMPTY) {
+        result = { row: curr.row, col: c };
+      }
+    }
+    return result;
+  }
+
   swap(
     map: string[][],
     obj1: { row: number; col: number },
@@ -100,6 +239,32 @@ O.#..O.#.#
     map[obj1.row][obj1.col] = map[obj2.row][obj2.col];
     map[obj2.row][obj2.col] = temp;
     return map;
+  }
+
+  getRounded(map: string[][]): Set<string> {
+    const set = new Set<string>();
+    for (let r = 0; r < map.length; r++) {
+      for (let c = 0; c < map[0].length; c++) {
+        if (map[r][c] === ROUNDED) set.add(this.formatLoc({ row: r, col: c }));
+      }
+    }
+    return set;
+  }
+
+  areSetsEqual(set1: Set<string> | null, set2: Set<string> | null) {
+    if (set1 === null || set2 === null) return false;
+
+    if (set1.size !== set2.size) {
+      return false;
+    }
+
+    for (const item of set1) {
+      if (!set2.has(item)) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   formatLoc(currPos: { row: number; col: number }) {
