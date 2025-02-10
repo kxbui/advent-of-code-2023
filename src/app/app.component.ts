b0f4463d25260d0a32734e3f3e9a0b1be6023587
Khoi Bui <emquacau123472@yahoo.com>
2025-02-10 09:04:52 -0500

    day 21 part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject, NgZone, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-const BTN_PUSH_NUM = 1000;
+// const NUM_STEPS = 64;
+const NUM_STEPS = 6;
 
 @Component({
   selector: 'app-root',
@@ -11,17 +12,17 @@ const BTN_PUSH_NUM = 1000;
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-//   input = `broadcaster -> a
-// %a -> inv, con
-// &inv -> b
-// %b -> con
-// &con -> output`;
-
-  input = `broadcaster -> a, b, c
-%a -> b
-%b -> c
-%c -> inv
-&inv -> a`;
+  input = `...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........`;
 
   result = signal('');
   ngZone = inject(NgZone);
@@ -31,150 +32,75 @@ export class AppComponent {
 
     this.ngZone.runOutsideAngular(() => {
       setTimeout(() => {
-        const data = this.parseRow(this.input);
+        const data = this.parseRow(this.input).map((line) => line.split(''));
         const total = this.start(data);
         this.result.set(`${total}`);
       }, 0);
     });
   }
 
-  start(data: string[]): number {
-    let totalHigh = 0,
-      totalLow = 0;
-    const { configs, ffSet, conjSet } = this.parseInput(data);
-    const ffMap = this.initFFMap(ffSet);
-    const conjMap = this.initConjMap(configs, conjSet);
+  start(map: string[][]): number {
+    const start = this.findStartPosition(map);
 
-    Array(BTN_PUSH_NUM)
-      .fill('')
-      .forEach(() => {
-        const { high, low } = this.runSeq(configs, ffMap, conjMap);
-        totalHigh += high;
-        totalLow += low;
-      });
-    return totalHigh * totalLow;
+    if (start) {
+      return this.move(map, [start], 0);
+    }
+    return 0;
   }
 
-  runSeq(
-    configs: Map<string, string[]>,
-    ffMap: Map<string, string>,
-    conjMap: Map<string, Map<string, string>>
-  ) {
-    let high = 0,
-      low = 1;
-    const queue = [];
-    queue.push('broadcaster-L');
+  move(map: string[][], startNodes: any[], stepCount: number): number {
+    let arr: any[] = [];
+    let set = new Set<string>();
 
-    while (queue.length) {
-      const config = queue.shift();
-      if (config) {
-        const [module, pulse] = config.split('-');
-        const destinations = configs.get(module);
-        if (destinations?.length) {
-          destinations.forEach((dest) => {
-            if (ffMap.has(dest)) {
-              const newPulse = this.handleFF(pulse, ffMap.get(dest)!);
-              if (newPulse) {
-                ffMap.set(dest, newPulse);
-                queue.push(`${dest}-${newPulse}`);
-              }
-            } else if (conjMap.has(dest)) {
-              const newMap = this.handleConj(
-                conjMap.get(dest)!,
-                module,
-                pulse
-              )!;
-              conjMap.set(dest, newMap);
-              const newPulse = Array.from(newMap.values()).every(
-                (item) => item === 'H'
-              )
-                ? 'L'
-                : 'H';
-              queue.push(`${dest}-${newPulse}`);
-            }
-            pulse === 'L' && low++;
-            pulse === 'H' && high++;
-          });
+    startNodes.forEach((node) => {
+      set = new Set([
+        ...set,
+        ...this.getNeighbors(map, node).map((loc) => this.formatLoc(loc)),
+      ]);
+    });
+
+    arr = Array.from(set.values()).map((str) => {
+      const [r, c] = str.split('-');
+      return { row: Number(r), col: Number(c) };
+    });
+
+    if (stepCount === NUM_STEPS - 1) {
+      return arr.length;
+    }
+
+    return this.move(map, arr, stepCount + 1);
+  }
+
+  getNeighbors(map: string[][], curr: { row: number; col: number }): any[] {
+    return [
+      // left
+      { row: curr.row, col: curr.col - 1 },
+      // right
+      { row: curr.row, col: curr.col + 1 },
+      // top
+      { row: curr.row - 1, col: curr.col },
+      // bottom
+      { row: curr.row + 1, col: curr.col },
+    ].filter((loc) => !this.isRock(map, loc));
+  }
+
+  isRock(map: string[][], curr: { row: number; col: number }) {
+    return map[curr.row][curr.col] === '#';
+  }
+
+  findStartPosition(map: string[][]): { row: number; col: number } | null {
+    for (let row = 0; row < map.length; row++) {
+      for (let col = 0; col < map[0].length; col++) {
+        if (map[row][col] === 'S') {
+          return { row, col };
         }
       }
-    }
-    return { low, high };
-  }
-
-  initFFMap(ffSet: Set<string>) {
-    return new Map(Array.from(ffSet.values()).map((key) => [key, 'L']));
-  }
-
-  initConjMap(
-    configs: Map<string, string[]>,
-    conjSet: Set<string>
-  ) {
-    const map = new Map<string, Map<string, string>>();
-    Array.from(configs.entries())
-      .forEach(([input, arr]) => {
-        arr.forEach((item) => {
-          if (conjSet.has(item)) {
-            map.has(item)
-              ? map.set(item, new Map([...map.get(item)!, [input, 'L']]))
-              : map.set(item, new Map([[input, 'L']]));
-          }
-        });
-      });
-
-    return map;
-  }
-
-  handleFF(pulse: string, prev: string): string | null {
-    if (pulse === 'L') {
-      return prev === 'L' ? 'H' : 'L';
     }
     return null;
   }
 
-  handleConj(
-    map: Map<string, string>,
-    module: string,
-    pulse: string
-  ): Map<string, string> {
-    const newMap = new Map(map);
-    newMap.set(module, pulse);
-    return newMap;
-  }
-
-  parseInput(data: string[]): {
-    configs: Map<string, string[]>;
-    ffSet: Set<string>;
-    conjSet: Set<string>;
-  } {
-    const configs = new Map<string, string[]>();
-    const ffSet = new Set<string>();
-    const conjSet = new Set<string>();
-
-    data.forEach((line) => {
-      const idx1 = line.indexOf('-');
-      const idx2 = line.indexOf('>');
-      const module = line
-        .substring(
-          ['%', '&'].some((char) => line.startsWith(char)) ? 1 : 0,
-          idx1
-        )
-        .trim();
-      configs.set(
-        module,
-        line
-          .substring(idx2 + 1)
-          .split(',')
-          .map((str) => str.trim())
-      );
-      line.startsWith('%') && ffSet.add(module);
-      line.startsWith('&') && conjSet.add(module);
-    });
-
-    return { configs, ffSet, conjSet };
-  }
-
-  formatLoc(currPos: { row: number; col: number; direction: string }) {
-    return [currPos.row, currPos.col, currPos.direction]
+  formatLoc(currPos: { row: number; col: number }) {
+    return [currPos.row, currPos.col]
       .filter((str) => str !== undefined)
       .join('-');
   }
